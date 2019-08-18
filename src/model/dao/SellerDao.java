63fdfc85fc37ec03a8876035e84e51af69841196
Felipe Salles <prof.felipe.salles@gmail.com>
2019-08-18 12:22:11 -0300

    Insert dao classes

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -3,14 +3,14 @@ package model.dao;
 import java.util.List;
 
 import model.entities.Department;
+import model.entities.Seller;
 
-public interface DepartmentDao {
-	
+public interface SellerDao {
 	void insert(Department obj);
 	void update(Department obj);
 	void deleteById(Department obj);
 	Department findById(Integer id);
 	List<Department> findAll();
-	
+	List<Seller> findByDepartment(Department department);
 	
 }
